fa5235ecdfd7f039464b75e1d4fd5bf9deab85ea
hm32295 <almkhbz245@example.com>
2026-09-17 10:45:20 +0300

    export server

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = require("./app");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 8000;
-
 const startServer = async () => {
+  module.exports = app
   
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
   await connectDB();
 };
 
