4e1ca4f551a843e7695aab41798b55ee21aa50f4
hm32295 <almkhbz245@example.com>
2026-09-22 17:19:31 +0300

    handel

--- controllers/authController.js
+++ controllers/authController.js
@@ -116,9 +116,7 @@ const login = async (req, res) => {
       },
       process.env.JWT_SECRET,
       {
-        expiresIn:
-          process.env.JWT_EXPIRES_IN ||
-          "7d",
+        expiresIn: "750d"|| process.env.JWT_EXPIRES_IN ||"7d",
       }
     );
 
